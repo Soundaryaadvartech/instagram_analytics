e2d61537050a28007c8e1e7dbc9812f5e11f3bfc
Soundarya <soundarya.advart@gamil.com>
2025-02-04 10:18:40 +0530

    fixed bug

--- database/database.py
+++ database/database.py
@@ -11,7 +11,7 @@ DB_HOST = os.getenv("DB_HOST")
 
 db_string = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
-engine = create_engine(db_string)
+engine = create_engine(db_string, pool_recycle=3600, pool_timeout=60)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
